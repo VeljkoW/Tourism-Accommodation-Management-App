93ee90e73a646951cd7d429e0553e0e5a2c435b7
srdjan <srdjanilic.002@gmail.com>
2024-03-09 18:20:40 +0100

    Location

---
+++ Model/Location.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookingApp.Serializer;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    public class Location : ISerializable
+    {
+        public int Id {  get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+
+        public Location() {
+            this.City = string.Empty;
+            this.State = string.Empty;
+        }
+        public Location(string city, string state)
+        {
+            this.City = city;
+            this.State = state;
+        }
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), State, City };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            State = values[1];
+            City = values[2];
+        }
+    }
+}
