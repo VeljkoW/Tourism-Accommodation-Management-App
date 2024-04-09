be41fe621f5616c91485ca632c2d4fa0a3b4148d
SanjaRadic23 <radic.sanja.rs@gmail.com>
2024-04-09 14:05:16 +0200

    Design for Guest Views

---
+++ Model/Converters/AccommodationConverter.cs
@@ -0,0 +1,28 @@
+﻿using BookingApp.Repository;
+using BookingApp.Repository.AccommodationRepositories;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BookingApp.Model.Converters
+{
+    public class AccommodationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int accommodationId = (int)value;
+            AccommodationRepository accommodationRepository = new AccommodationRepository();
+            Accommodation? accommodation = new Accommodation();
+            accommodation = accommodationRepository.GetById(accommodationId);
+            return accommodation;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
