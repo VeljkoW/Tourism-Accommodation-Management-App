adf34a630f093f62939c6ea54d195b3af8681139
srdjan <srdjanilic.002@gmail.com>
2024-04-12 17:31:06 +0200

    SuperOwner

--- View/Owner/GuestReviews.xaml.cs
+++ View/Owner/GuestReviews.xaml.cs
@@ -25,12 +25,12 @@ namespace BookingApp.View.Owner
     {
         public User User {  get; set; }
         public GuestReviewsViewModel GuestReviewsViewModel {  get; set; }
-        public OwnerRating SelectedOwnerRating {  get; set; }
+        public OwnerRating? SelectedOwnerRating {  get; set; }
         public GuestReviews(User User)
         {
             InitializeComponent();
             this.User = User;
-            GuestReviewsViewModel = new GuestReviewsViewModel(User, SelectedOwnerRating);
+            GuestReviewsViewModel = new GuestReviewsViewModel(this, User, SelectedOwnerRating);
             DataContext = GuestReviewsViewModel;
         }
     }
