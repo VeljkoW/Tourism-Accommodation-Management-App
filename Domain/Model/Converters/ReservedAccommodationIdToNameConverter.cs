71d6503a059dacf55ca0ebba56bb12508a33cfa4
SanjaRadic23 <radic.sanja.rs@gmail.com>
2024-05-25 01:17:36 +0200

    hci

---
+++ Domain/Model/Converters/ReservedAccommodationIdToNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using BookingApp.Domain.Model;
+using BookingApp.Repository;
+using BookingApp.Repository.AccommodationRepositories;
+using BookingApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BookingApp.Domain.Model.Converters
+{
+    public class ReservedAccommodationIdToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int reservedAccommodationId = (int)value;
+            ReservedAccommodation? reservedAccommodation = new ReservedAccommodation();
+            reservedAccommodation = ReservedAccommodationService.GetInstance().GetById(reservedAccommodationId);
+            return reservedAccommodation.Accommodation.Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
