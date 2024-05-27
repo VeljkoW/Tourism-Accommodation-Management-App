0e2de9e3b931f0b7c4661369655f47741a546c6a
Vojnic <vojnicnemanja@gmail.com>
2024-05-27 19:03:39 +0200

    didn't add everything

---
+++ View/Guide/Pages/BurgerMenu.xaml.cs
@@ -0,0 +1,64 @@
+﻿using BookingApp.ViewModel.Guide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookingApp.View.Guide.Pages
+{
+    /// <summary>
+    /// Interaction logic for BurgerMenu.xaml
+    /// </summary>
+    public partial class BurgerMenu : UserControl
+    {
+        private GuideMainPage page;
+        public BurgerMenu(GuideMainPage page)
+        {
+            InitializeComponent();
+            this.page = page;
+        }
+        private void UpcommingTours(object sender, RoutedEventArgs e)
+        {
+            page.ClickUpcommingTour(sender, e);
+        }
+        private void CreateTour(object sender, RoutedEventArgs e)
+        {
+            page.ClickCreateTour(sender, e);
+        }
+        private void TourRequests(object sender, RoutedEventArgs e)
+        {
+            page.ClickTourSuggestions(sender, e);
+        }
+        private void TourStatistics(object sender, RoutedEventArgs e)
+        {
+            page.ClickTourStatistics(sender, e);
+        }
+        private void TourRequestStatistics(object sender, RoutedEventArgs e)
+        {
+            page.ClickTourSuggestionsStatistics(sender, e);
+        }
+        private void ComplexRequests(object sender, RoutedEventArgs e)
+        {
+            //TODO: VELJKO
+            page.ClickTourSuggestions(sender, e);
+        }
+        private void FinishedTours(object sender, RoutedEventArgs e)
+        {
+            page.ClickTourReviews(sender, e);
+        }
+        private void HideBurger(object sender, RoutedEventArgs e)
+        {
+            page.HideBurger(sender, e);
+        }
+    }
+}
---
+++ ViewModel/Guide/UpcomingTourViewModel.cs
@@ -0,0 +1,71 @@
+﻿using BookingApp.Domain.Model;
+using BookingApp.Services;
+using BookingApp.View.Guide.Pages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BookingApp.ViewModel.Guide
+{
+    public class UpcomingTourViewModel : INotifyPropertyChanged
+    {
+        public UpcomingTourViewModel(User user)
+        {
+            User = user;
+            UserName = user.Username;
+            TourList = new ObservableCollection<UserControlTourCard>();
+            Load();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public User User { get; set; }
+        public string UserName { get; set; }
+        public List<Tour> Tours { get; set; }
+        private ObservableCollection<UserControlTourCard> _tourList;
+        public ObservableCollection<UserControlTourCard> TourList
+        {
+            get { return _tourList; }
+            set
+            {
+                _tourList = value;
+                OnPropertyChanged(nameof(TourList));
+            }
+        }
+        public void Load()
+        {
+            TourList.Clear();
+            Dictionary<TourSchedule, Tour> t = TourService.GetInstance().LoadToursForGuide(User);
+            foreach (var entry in t)
+            {
+                CreateTourCard(entry.Value, entry.Key);
+            }
+        }
+        private void CreateTourCard(Tour tour, TourSchedule schedule)
+        {
+            UserControlTourCard userControlTourCard = new UserControlTourCard(tour, User, schedule);
+            userControlTourCard.Margin = new Thickness(0, 0, 0, 15);
+            userControlTourCard.OnFinishedTour += UserControlTourCard_OnFinishedTour;
+            userControlTourCard.OnClickedGoBackMonitoringTour += UserControlTourCard_OnFinishedTour;
+            if (schedule.ScheduleStatus == ScheduleStatus.Ongoing)
+            {
+                TourList.Insert(0, userControlTourCard);
+            }
+            else
+            {
+            TourList.Add(userControlTourCard);
+            }
+        }
+        private void UserControlTourCard_OnFinishedTour()
+        {
+            Load();
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
